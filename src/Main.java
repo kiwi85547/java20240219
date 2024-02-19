97d3eb0b8971cabc2d97a63e2127e9c0f2574acb
Hwayeong <joid1004@naver.com>
2024-02-19 12:41:26 +0900

    처음 만들어본 자바 코드?!

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,10 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and 화영아아아");
+        System.out.println("Hi java");
+        System.out.println(2+3);
+        System.out.println("다형성 예제");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
---
+++ src/Main3.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println(1+2+" = 1+2");
+        System.out.println(57684);
+        System.out.println("K-digital Training 과정");
+        System.out.println("JAVA Web Application");
+        System.out.println("수강생 조화영");
+        System.out.println("점심 메뉴 정하기");
+    }
+}
